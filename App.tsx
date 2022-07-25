307da3d10f8f61915a8e0b2c4acb115bfe1c524d
Artur Moraes <artur.gomoraes@gmail.com>
2022-07-25 13:14:52 -0300

    criando botões de filtro

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { THEME } from './src/styles/theme'
-import { Signin } from './src/screens/Signin';
+import { Home } from './src/screens/Home';
 import{ Loading } from './src/components/Loading';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         translucent
       />
 
-      { fontsLoaded ? <Signin /> : <Loading /> }
+      { fontsLoaded ? <Home /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
